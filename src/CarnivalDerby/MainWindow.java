9e129883acccabe04f4adc87074aad870060eb4a
Zack Phoenix <nightphoenix13@hotmail.com>
2014-04-26 17:38:29 -0500

    Second commit. Added game panel contents. Need to work on event handling next. I also realized there is no way to choose the car to bet on. That will be coming next, as well.

--- src/CarnivalDerby/MainWindow.java
+++ src/CarnivalDerby/MainWindow.java
@@ -6,6 +6,10 @@
 package CarnivalDerby;
 
 import java.awt.*;
+import java.awt.image.*;
+import java.io.*;
+
+import javax.imageio.*;
 import javax.swing.*;
 
 public class MainWindow extends JFrame // MainWindow class start
@@ -14,7 +18,8 @@ public class MainWindow extends JFrame // MainWindow class start
 	private JPanel headerPanel,
 				   ioPanel,
 				   wagerPanel,
-				   totalPanel;
+				   totalPanel,
+				   gamePanel;
 	private JMenu game,
 				  help;
 	private JMenuItem reset,
@@ -26,7 +31,19 @@ public class MainWindow extends JFrame // MainWindow class start
 	private JTextField wager,
 					   total;
 	private JLabel wagerLabel,
-				   totalLabel;
+				   totalLabel,
+				   car1Label,
+				   car2Label,
+				   car3Label,
+				   car4Label,
+				   car5Label,
+				   finishLineLabel;
+	private BufferedImage car1,
+						  car2,
+						  car3,
+						  car4,
+						  car5,
+						  finishLine;
 	
 	public MainWindow() // constructor start
 	{
@@ -34,9 +51,11 @@ public class MainWindow extends JFrame // MainWindow class start
 		setLayout(new BorderLayout());
 		buildHeaderPanel();
 		buildIOPanel();
+		buildGamePanel();
 		
 		// adding panels to frame
 		add(headerPanel, BorderLayout.NORTH);
+		add(gamePanel, BorderLayout.CENTER);
 		add(ioPanel, BorderLayout.SOUTH);
 	} // constructor end
 	
@@ -112,4 +131,49 @@ public class MainWindow extends JFrame // MainWindow class start
 		ioPanel.add(start);
 		ioPanel.add(wagerPanel);		
 	} // buildIOPanel method end
+	
+	// buildGamePanel method builds the gamePanel
+	private void buildGamePanel() // buildGamePanel method start
+	{
+		// creating components
+		gamePanel = new JPanel();
+		try
+		{
+			car1 = ImageIO.read(new File("Car1.jpg"));
+			car2 = ImageIO.read(new File("Car2.jpg"));
+			car3 = ImageIO.read(new File("Car3.jpg"));
+			car4 = ImageIO.read(new File("Car4.jpg"));
+			car5 = ImageIO.read(new File("Car5.jpg"));
+			finishLine = ImageIO.read(new File("FinishLine.jpg"));
+		} // end try
+		catch (IOException e)
+		{
+			JOptionPane.showMessageDialog(gamePanel, "Error opening image file(s).", "File Error",
+					JOptionPane.ERROR_MESSAGE);
+		} // end catch
+		car1Label = new JLabel(new ImageIcon(car1));
+		car2Label = new JLabel(new ImageIcon(car2));
+		car3Label = new JLabel(new ImageIcon(car3));
+		car4Label = new JLabel(new ImageIcon(car4));
+		car5Label = new JLabel(new ImageIcon(car5));
+		finishLineLabel = new JLabel(new ImageIcon(finishLine));
+		
+		// component properties
+		gamePanel.setBackground(Color.WHITE);
+		gamePanel.setLayout(null);
+		car1Label.setBounds(20, 20, 150, 75);
+		car2Label.setBounds(20, 140, 150, 75);
+		car3Label.setBounds(20, 260, 150, 75);
+		car4Label.setBounds(20, 380, 150, 75);
+		car5Label.setBounds(20, 500, 150, 75);
+		finishLineLabel.setBounds(900, 20, 40, 600);
+		
+		// adding components to panel
+		gamePanel.add(car1Label);
+		gamePanel.add(car2Label);
+		gamePanel.add(car3Label);
+		gamePanel.add(car4Label);
+		gamePanel.add(car5Label);
+		gamePanel.add(finishLineLabel);
+	} // buildGamePanel method end
 } // MainWindow class end
